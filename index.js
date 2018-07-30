f4e05752c90374f89eaa5f45d27f3f1457f3b2de
Ethan Murphy <verakai141@gmail.com>
2018-07-30 16:21:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,16 +15,15 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string){
-    if (string.toLowerCase() === string){
-      return 'I can\'t hear you!';
     
-      
+ if (string === 'I love you, Grandma.'){
+      return 'I love you, too.';
+    
     }else if (string.toUpperCase() === string){
       return 'YES INDEED!';
     
-    }else if (string === 'I love you, Grandma.'){
-      return 'I love you, too.';
-    }
-}
-  
+    } else if (string.toLowerCase() === string){
+      return 'I can\'t hear you!';
+  }
+}  
  
